c15aa79bf4ea20df7c8d896e70516273ce5a56d4
MariiieP <33463825+MariiieP@users.noreply.github.com>
2020-01-04 13:05:17 +0300

    first settings

---
+++ pazzle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    //private GameManager() { }
+
+    private List<MiniBlock> _blocks = new List<MiniBlock>();
+
+    public void AddBlock(MiniBlock block)
+    {
+        _blocks.Add(block);
+    }
+
+    public float DistToWin = 5f;
+    public GameObject WinWindow;
+
+    private void Update()
+    {
+        if (CheckWin())
+        {
+            if (WinWindow != null)
+            {
+                WinWindow.SetActive(true);
+            }
+        }
+    }
+
+    public bool CheckWin()
+    {
+        var nullPosition = _blocks.Find(x => x.IsMine).StartedPosition;
+        int countWinBlock = 0;
+        for(int i = 0; i < _blocks.Count; i++)
+        {
+            var distToWin = Vector3.Distance(_blocks[i].transform.position - nullPosition, _blocks[i].StartedPosition- nullPosition);
+            if(distToWin < DistToWin)
+            {
+                countWinBlock++;
+            }
+        }
+        if(countWinBlock >= _blocks.Count)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ pazzle/Assets/Scripts/MiniBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBlock : MonoBehaviour
+{
+    public bool IsMine = false;
+    public Vector3 StartedPosition;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartedPosition = transform.position;
+        GameManager.Instance.AddBlock(this);
+        transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.CheckWin())
+        {
+            Debug.Log("We win");
+        }
+    }
+}

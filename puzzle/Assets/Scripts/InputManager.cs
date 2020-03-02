7f348dd3a2da298f21c55a7590e2e94dc663f272
MariiieP <33463825+MariiieP@users.noreply.github.com>
2020-03-02 14:07:14 +0300

    fix scetch

---
+++ puzzle/Assets/Scripts/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private bool _isMove = false;
+    private Transform _miniblockTransform=null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool buttonClick = Input.GetMouseButton(0);
+        if (buttonClick)
+        {
+            Vector2 mousePosition = Input.mousePosition;
+
+            Vector2 currentWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            //_transform.position = currentWorldPosition;
+
+            //RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, );
+            RaycastHit2D rayHit = Physics2D.Raycast(currentWorldPosition, Vector2.zero);
+
+            if (rayHit.transform != null)
+            {
+                var go = rayHit.transform.gameObject;
+                var MiniBlock = go.GetComponent<MiniBlock>();
+
+                if (MiniBlock != null)
+                {
+                    _miniblockTransform=go.transform;
+                    _isMove=true;
+                }
+            }
+
+            if (_isMove && _miniblockTransform != null)
+            {
+                _miniblockTransform.position = currentWorldPosition;
+            }
+        }
+        else
+        {
+            _isMove = false;
+        }
+    }
+
+
+}

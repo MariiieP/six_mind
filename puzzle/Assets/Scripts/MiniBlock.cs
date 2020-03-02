f4f9922c77f75585fd35e2c5fbddf834fd7ac89e
MariiieP <33463825+MariiieP@users.noreply.github.com>
2020-03-02 20:43:55 +0300

    fix scetch

--- puzzle/Assets/Scripts/MiniBlock.cs
+++ puzzle/Assets/Scripts/MiniBlock.cs
@@ -7,20 +7,11 @@ public class MiniBlock : MonoBehaviour
     public bool IsMine = false;
     public Vector3 StartedPosition;
 
+    public List<MiniBlock> allBlocks = new List<MiniBlock>();
     // Start is called before the first frame update
     private void Start()
     {
         StartedPosition = transform.position;
-        GameManager.Instance.AddBlock(this);
         transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (GameManager.Instance.CheckWin())
-        {
-            Debug.Log("We win");
-        }
     }
 }

d56962e03b5a019bf627d6c48cf87d32bbed0854
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2020-03-04 20:01:08 +0300

    primary refactoring

---
+++ puzzle/Assets/Scripts/LetterData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLetterData", menuName = "ScriptableObjects/LetterData")]
+public class LetterData : ScriptableObject
+{
+	[Serializable]
+	public struct Object
+	{
+		[SerializeField] public GameObject Prefab;
+		[SerializeField] public Vector3 Position;
+		[SerializeField] public Vector3 Rotation;
+	}
+
+	public Object[] Letters;
+}
--- puzzle/Assets/Scripts/MiniBlock.cs
+++ puzzle/Assets/Scripts/MiniBlock.cs
@@ -1,17 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class MiniBlock : MonoBehaviour
 {
-    public bool IsMine = false;
-    public Vector3 StartedPosition;
+	public bool IsMine = false;
+	[HideInInspector] public Vector3 StartedPosition;
 
-    public List<MiniBlock> allBlocks = new List<MiniBlock>();
-    // Start is called before the first frame update
-    private void Start()
-    {
-        StartedPosition = transform.position;
-        transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0);
-    }
+	public List<MiniBlock> allBlocks = new List<MiniBlock>();
+
+	private void Start()
+	{
+		StartedPosition = transform.position;
+		transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0);
+	}
 }

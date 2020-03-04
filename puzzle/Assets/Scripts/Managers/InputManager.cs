dc1423ff3e57e3e4481a265166f240753e0b2134
MariiieP <33463825+MariiieP@users.noreply.github.com>
2020-03-04 20:01:08 +0300

    primary refactoring

---
+++ puzzle/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+	public class InputManager : MonoBehaviour
+	{
+		private bool _isMoving = false;
+		private Transform _targetTransform = null;
+
+		private void Update()
+		{
+			if (Input.GetMouseButton(0))
+			{
+				var mousePosition = Input.mousePosition;
+				var currentWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+				var rayHit = Physics2D.Raycast(currentWorldPosition, Vector2.zero);
+
+				if (rayHit.transform != null)
+				{
+					var go = rayHit.transform.gameObject;
+					var component = go.GetComponent<MiniBlock>();
+
+					if (component != null)
+					{
+						_targetTransform = go.transform;
+						_isMoving = true;
+					}
+				}
+
+				if (_isMoving && _targetTransform != null)
+				{
+					currentWorldPosition.z = 0;
+					_targetTransform.position = currentWorldPosition;
+				}
+			}
+			else
+			{
+				_isMoving = false;
+			}
+		}
+	}
+}

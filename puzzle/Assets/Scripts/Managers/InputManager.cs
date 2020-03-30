69419cce5190de87494acc63ae909a6f46e438e7
Mari <33463825+MariiieP@users.noreply.github.com>
2020-03-30 17:57:48 +0300

    Mouse rotation

--- puzzle/Assets/Scripts/Managers/InputManager.cs
+++ puzzle/Assets/Scripts/Managers/InputManager.cs
@@ -3,68 +3,133 @@ using UnityEngine;
 
 namespace Managers
 {
+	[RequireComponent(typeof(Rigidbody2D))]
+	[RequireComponent(typeof(Collider2D))]
 	public class InputManager : MonoBehaviour
 	{
+		private enum MouseButtons { Left = 0, Right = 1 }
+
 		[SerializeField] private Camera _camera;
 
 		private bool _targetCaptured = false;
 		private bool _isMoving = false;
+		private bool _isRotating = false;
 		private LetterPart _target = null;
-		private Vector3 _targetDelta = new Vector2(0, 0);
+		private Vector3 _positionDelta = new Vector2(0, 0);
+		private Vector3 _onScreenPosition = new Vector2(0, 0);
+		private float _angleDelta = 0f;
 
 		private void Update()
 		{
-			if (Input.GetMouseButton(0))
+			if (Input.GetMouseButton((int)MouseButtons.Left))
 			{
-				var mousePosition = Input.mousePosition;
-				var worldPosition = _camera.ScreenToWorldPoint(mousePosition);
-
-				if (_targetCaptured)
-				{
-					if (!_isMoving)
-					{
-						_targetDelta = worldPosition - _target.transform.position;
-						_target.body.constraints = RigidbodyConstraints2D.FreezeRotation;
-						_isMoving = true;
-
-					}
-					else
-					{
-						_target.body.MovePosition(worldPosition - _targetDelta);
-					}
-				}
-				else
-				{
-					var rayHit = Physics2D.Raycast(worldPosition, Vector2.zero);
-					if (rayHit.transform == null)
-					{
-						return;
-					}
-
-					var letterPart = rayHit.transform.GetComponent<LetterPart>();
-
-					if (letterPart == null)
-					{
-						return;
-					}
-
-					_target = letterPart;
-					_targetCaptured = true;
-				}
+				Manage(MouseButtons.Left);
 			}
 
-			if (Input.GetMouseButtonUp(0))
+			if (Input.GetMouseButton((int)MouseButtons.Right))
 			{
-				if (_target != null)
+				Manage(MouseButtons.Right);
+			}
+
+			if (Input.GetMouseButtonUp((int)MouseButtons.Left) 
+				|| Input.GetMouseButtonUp((int)MouseButtons.Right))
+			{
+				DropTarget();
+			}
+		}
+
+		private void Manage(MouseButtons button)
+		{
+			if (_targetCaptured)
+			{
+				if (button == MouseButtons.Left)
 				{
-					_target.body.constraints =
-						RigidbodyConstraints2D.FreezePositionX
-						| RigidbodyConstraints2D.FreezePositionY
-						| RigidbodyConstraints2D.FreezeRotation;
-					_target = null;
-					_isMoving = false;
-					_targetCaptured = false;
+					MoveTarget();
 				}
+				else if (button == MouseButtons.Right)
+				{
+					RotateTarget();					
+				}
+			}
+			else
+			{
+				CaptureTarget();
+			}
+		}
+
+		private Vector3 MouseWorldPosition
+		{
+			get
+			{
+				var mousePosition = Input.mousePosition;
+				return _camera.ScreenToWorldPoint(mousePosition);
+			}
+		}
+
+		private void MoveTarget()
+		{
+			if (!_isMoving)
+			{
+				_positionDelta = MouseWorldPosition - _target.transform.position;
+				_target.body.constraints = RigidbodyConstraints2D.FreezeRotation;
+				_isMoving = true;
+
+			}
+			else
+			{
+				_target.body.MovePosition(MouseWorldPosition - _positionDelta);
+			}
+		}
+
+		private void RotateTarget()
+		{
+			if (!_isRotating)
+			{
+				_onScreenPosition = _camera.WorldToScreenPoint(_target.transform.position);
+				var delta = Input.mousePosition - _onScreenPosition;
+				_angleDelta = (Mathf.Atan2(_target.transform.right.y, _target.transform.right.x) - Mathf.Atan2(delta.y, delta.x)) * Mathf.Rad2Deg;
+				_target.body.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+				_isRotating = true;
+			}
+			else
+			{
+				var delta = Input.mousePosition - _onScreenPosition;
+				float angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+				_target.body.MoveRotation(angle + _angleDelta);
+			}
+		}
+
+		private void CaptureTarget()
+		{
+			var rayHit = Physics2D.Raycast(MouseWorldPosition, Vector2.zero);
+			if (rayHit.transform == null)
+			{
+				return;
+			}
+
+			var letterPart = rayHit.transform.GetComponent<LetterPart>();
+
+			if (letterPart == null)
+			{
+				return;
+			}
+
+			_target = letterPart;
+			_targetCaptured = true;
+		}
+
+		private void DropTarget()
+		{
+			if (_target != null)
+			{
+				_target.body.constraints =
+					RigidbodyConstraints2D.FreezePositionX
+					| RigidbodyConstraints2D.FreezePositionY
+					| RigidbodyConstraints2D.FreezeRotation;
+				_target = null;
+				_isMoving = false;
+				_isRotating = false;
+				_targetCaptured = false;
 			}
 		}
 	}

ab99def38ece36b4966897acc0ca13d0d670d958
MariiieP <33463825+MariiieP@users.noreply.github.com>
2020-05-10 18:24:10 +0300

    fkjbvfkv

---
+++ puzzle/Assets/Scripts/Utils/PlayerDataRemover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class PlayerDataRemover : MonoBehaviour
+{
+    public void Remove()
+    {
+        PlayerData.ClearRestoreData();
+    }
+}

a098f5cc15a46de9f7facfa6d62e4f43ecf2b9d8
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2020-05-13 18:30:52 +0300

    Game Scene Layout

--- puzzle/Assets/Scripts/Managers/SessionManager.cs
+++ puzzle/Assets/Scripts/Managers/SessionManager.cs
@@ -6,14 +6,13 @@ namespace Managers
 {
 	public class SessionManager : MonoBehaviourSingleton<SessionManager>
 	{
-		//[SerializeField] private Letter _letterPrefab;
 		[SerializeField] private GameObject _boundsPrefab;
 		[SerializeField] private Transform _spawnPoint;
 		[SerializeField] private float _distanceDelta;
 		[SerializeField] private float _rotationDelta;
+		public RectTransform[] BoundsCoordinates;
 
 		private Letter _letterInstance;
-
 
 		private void Start()
 		{
@@ -61,7 +60,7 @@ namespace Managers
 			return true;
 		}
 
-		private void StartCleanSession()
+		public void StartCleanSession()
 		{
 			var sessionData = PlayerData.SessionConfiguration;
 
@@ -126,23 +125,23 @@ namespace Managers
 				current.NeighbourRotation = neighbourRotation;
 				current.NeighbourDistance = neighbourDistance;
 
-				Debug.Log($"Current: {current.name} -> Neighbor: {neighbour.name}, rotation: {neighbourRotation}, distance: {neighbourDistance}");
+				//Debug.Log($"Current: {current.name} -> Neighbor: {neighbour.name}, rotation: {neighbourRotation}, distance: {neighbourDistance}");
 			}
 		}
 
 		private void SetupBounds()
 		{
-			Vector2[] boundsPositions = new Vector2[4];
-			boundsPositions[0] = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)); // lowerLeft
-			boundsPositions[1] = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)); // upperRight
-			boundsPositions[2] = new Vector2(boundsPositions[1].x, boundsPositions[0].y); // lowerRight
-			boundsPositions[3] = new Vector2(boundsPositions[0].x, boundsPositions[1].y); // upperLeft
+			//Vector2[] boundsPositions = new Vector2[4];
+			//boundsPositions[0] = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)); // lowerLeft
+			//boundsPositions[1] = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)); // upperRight
+			//boundsPositions[2] = new Vector2(boundsPositions[1].x, boundsPositions[0].y); // lowerRight
+			//boundsPositions[3] = new Vector2(boundsPositions[0].x, boundsPositions[1].y); // upperLeft
 
 			var bounds = Instantiate(_boundsPrefab);
 			for (int i = 0; i < bounds.transform.childCount; ++i)
 			{
 				var child = bounds.transform.GetChild(i);
-				child.position = boundsPositions[i];
+				child.position = BoundsCoordinates[i].transform.position;
 			}
 		}
 	}

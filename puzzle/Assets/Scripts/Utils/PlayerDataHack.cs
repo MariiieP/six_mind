24c832348f95ba88c67332b0ef10d6546cde9835
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2020-05-31 21:20:23 +0300

    Cheat buttons

---
+++ puzzle/Assets/Scripts/Utils/PlayerDataHack.cs
@@ -0,0 +1,30 @@
+﻿using App;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils;
+
+namespace Utils {
+    public class PlayerDataHack : MonoBehaviour
+    {
+        public void Remove()
+        {
+            PlayerData.ClearRestoreData();
+            SceneManager.LoadScene("Preloader");
+        }
+
+        public void UnlockProgress()
+        {
+            var levelsConfig = AppController.Instance.LevelsConfig;
+            var progressData = AppController.Instance.GetProgressData();
+            progressData.CompletedLevelIds.Clear();
+            foreach (var cfg in levelsConfig.Levels)
+            {
+                progressData.CompletedLevelIds.Add(cfg.LevelId);
+            }
+            AppController.Instance.SaveProgressData(progressData);
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
